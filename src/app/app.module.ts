6575c1d45f805a681480e290f664b42866e85e7c
Mohit Taunk <mohit.taunk@intellinum.com>
2020-07-05 15:45:09 +0530

    add task and list functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { TaskBoardComponent } from './task-board/task-board.component';
 import {DragDropModule} from '@angular/cdk/drag-drop';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,10 @@ import {DragDropModule} from '@angular/cdk/drag-drop';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DragDropModule
+    ReactiveFormsModule,
+    FormsModule,
+    DragDropModule,
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/task-board/task-board.component.ts
+++ src/app/task-board/task-board.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Board } from '../models/board.model';
 import { Column } from '../models/column.model';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-task-board',
@@ -9,43 +11,134 @@ import { Column } from '../models/column.model';
   styleUrls: ['./task-board.component.css']
 })
 export class TaskBoardComponent implements OnInit {
+  cardName = '';
+  cardNameInput = '';
+  cardNameError = '';
+  listName = '';
+  listNameInput = '';
+  listNameError = '';
+  deleteData = {};
+  public modalRef: BsModalRef;
+  config = {
+    backdrop: true,
+    ignoreBackdropClick: true
+  };
+  board: Board = new Board('Sample Application', []);
+  constructor(private modalService: BsModalService) { }
 
-  constructor() { }
-
-  board: Board = new Board('Sample Application', [
-    new Column('To Do', [
-      'Pay electricity bill',
-      'Make grocery list'
-    ]),
-    new Column('In Progress', [
-      'Iron clothes',
-    ]),
-    new Column('Done', [
-      'Buy running shoes'
-    ])
-  ]);
 
   ngOnInit() {
-    if (!localStorage.getItem('data')){
+    if (localStorage.getItem('data') === null) {
+      this.board.columns.push(
+        new Column('To Do', [
+          'Pay electricity bill',
+          'Make grocery list'
+        ]),
+        new Column('In Progress', [
+          'Iron clothes',
+        ]),
+        new Column('Done', [
+          'Buy running shoes'
+        ])
+      );
       localStorage.setItem('data', JSON.stringify(this.board));
+    } else {
+      this.board = JSON.parse(localStorage.getItem('data'));
     }
   }
 
-  // add a task
-  addTask(columnName){
-    console.log(columnName);
-    for (const column of this.board.columns){
-      if (column.name === columnName){
-        column.tasks.push('New Task');
+  // open model
+  openModal(template: TemplateRef<any>, type: string, name?: string) {
+    if (type === 'card') {
+      this.cardName = name;
+    } else{
+      this.listName = name;
+    }
+    this.modalRef = this.modalService.show(template, this.config);
+  }
+
+  // open delete model
+  openDeleteModel(template: TemplateRef<any>, cardName, item?: string) {
+    this.deleteData = {cardName, item};
+    this.modalRef = this.modalService.show(template, this.config);
+  }
+
+  // check name
+  checkName(type) {
+    if (type === 'card') {
+      if (this.cardNameInput === ''){
+        this.cardNameError = 'Please enter card name';
+      } else {
+        this.cardNameError = '';
+      }
+    } else {
+      if (this.listNameInput === ''){
+        this.listNameError = 'Please enter list name';
+      } else {
+        this.listNameError = '';
       }
     }
   }
 
-  // add a list
-  addList(){
-    console.log('add list');
+  // add a task
+  addCard() {
+    // console.log(columnName);
+    if (this.cardNameInput === '') {
+      this.cardNameError = 'Please enter card name';
+      return;
+    }
+    for (const column of this.board.columns) {
+      if (column.name === this.cardName) {
+        this.modalRef.hide();
+        column.tasks.push(this.cardNameInput);
+        this.cardNameInput = '';
+        this.cardNameError = '';
+        localStorage.setItem('data', JSON.stringify(this.board));
+        return;
+      }
+    }
   }
 
+  // delete card
+  deleteCard(data) {
+    for (const column of this.board.columns) {
+      if (column.name === data.cardName) {
+        this.modalRef.hide();
+        const taskIndex = column.tasks.indexOf(data.item);
+        column.tasks.splice(taskIndex, 1);
+        this.deleteData = {};
+        localStorage.setItem('data', JSON.stringify(this.board));
+        return;
+      }
+    }
+  }
+
+  // add list
+  addList() {
+    if (this.listNameInput === '') {
+      this.listNameError = 'Please enter list name';
+      return;
+    }
+
+    this.modalRef.hide();
+    const newColumn = new Column(this.listNameInput,  []);
+    this.board.columns.push(newColumn);
+    this.listNameInput = '';
+    this.listNameError = '';
+    localStorage.setItem('data', JSON.stringify(this.board));
+  }
+
+  // delete list
+  deleteList(data) {
+    this.modalRef.hide();
+    let columns = [...this.board.columns];
+    columns = columns.filter(x => x.name !== data.cardName);
+    this.board.columns = columns;
+    this.deleteData = {};
+    localStorage.setItem('data', JSON.stringify(this.board));
+  }
+
+  // drop event
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
@@ -54,6 +147,7 @@ export class TaskBoardComponent implements OnInit {
         event.container.data,
         event.previousIndex,
         event.currentIndex);
+      localStorage.setItem('data', JSON.stringify(this.board));
     }
   }
 }
